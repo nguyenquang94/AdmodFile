29b6a39b1b7a612a5ada04c5a94c27f1b97814d9
LuyNTPH04174 <luyntph04174@fpt.edu.vn>
2017-08-20 14:18:33 +0700

    Them file 1708

---
+++ Admod16-1708/Src/ToiVaYeuEm/app/src/main/java/com/songtrao/toivayeuem/MainActivity.java
@@ -0,0 +1,19 @@
+package com.songtrao.toivayeuem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
